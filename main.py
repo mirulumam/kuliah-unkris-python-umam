61d2d3413857254e62b4a49d60c501131392adde
Chairul Umam <mirulumam@outlook.com>
2022-05-14 01:51:21 +0700

    Struktur Data: UTS

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 from helper import cls
+from strukturData.lab1 import lab1Pendahuluan, lab1Lanjutan, lab1Mandiri
+from strukturData.lab2 import lab2Pendahuluan, lab2Lanjutan, lab2Mandiri
 
 cls()
+
+lab1Pendahuluan.lab1Pendahuluan1()
+lab1Lanjutan.lab1TugasLanjutan2()
+lab1Lanjutan.lab1TugasLanjutan3()
+lab1Lanjutan.lab1TugasLanjutan4()
+lab1Mandiri.tugasMandiri5()
+lab1Mandiri.tugasMandiri9()
+lab1Mandiri.tugasMandiri10()
+
+lab2Pendahuluan.lab2Pendahuluan1()
+lab2Lanjutan.lab2TugasLanjutan2()
+lab2Lanjutan.lab2TugasLanjutan3()
+
+# please install regex package first on your laptop or pc
+# from command line before you continue
+#
+# command to install:
+# python -m pip install regex
+lab2Lanjutan.lab2TugasLanjutan4()
+lab2Mandiri.lab2TugasMandiri5()
+lab2Mandiri.lab2TugasMandiri6()
+lab2Mandiri.lab2TugasMandiri8()
