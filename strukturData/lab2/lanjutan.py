023dfb2a56f23cbc8888228fc78f85f68b1648a1
Chairul Umam <mirulumam@outlook.com>
2022-05-13 23:07:05 +0700

    Struktur Data: Bersih-bersih

--- strukturData/lab2/lanjutan.py
+++ strukturData/lab2/lanjutan.py
@@ -1,22 +1,23 @@
 import re
-from infixToPostfix import infixToPostfix
+from .infixPostfix import infixToPostfix
 
 class Lanjutan:
 
     # 2.6 Tugas Lanjutan (2)
-    def reverseList(list):
+    def reverseList(self, list):
         list.reverse()
         print(list)
 
     # 2.6 Tugas Lanjutan (3)
     # thanks to: https://stackoverflow.com/a/11230103
-    def htmlTagFinder():
+    def htmlTagFinder(self):
         html = "<html><head><title>Contoh Halaman Web</title></head><body><h1>Halo kawan, Saya belajar Python dan HTML</h1></body></html>"
         result = re.findall("<[^>]*>", html)
         print(result)
 
     # 2.6 Tugas Lanjutan (4)
-    def infixToPostfixResult():
+    def infixToPostfixResult(self):
         print(infixToPostfix("A * B + C * D"))
         print(infixToPostfix("( A + B ) * C - ( D - E ) * ( F + G )"))
+        print(infixToPostfix("(A+B)^C-((D+E)-(B*F))"))
 
