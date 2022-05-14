07a1fe812ccf79910484083817ace8966b6e3fd5
Chairul Umam <mirulumam@outlook.com>
2022-05-14 21:22:27 +0700

    Struktur Data: Lab. 1 - Tugas mandiri (6, 7, dan 8)

--- strukturData/lab1/lab1Mandiri.py
+++ strukturData/lab1/lab1Mandiri.py
@@ -18,6 +18,27 @@ def tugasMandiri5():
     except ValueError as e:
         print("ERROR", e.args[0])
 
+def tugasMandiri6():
+    a = F(1, 2)
+    b = F(2, 4)
+    c = a + b
+    print("__radd__", c.getNum(), "/", c.getDen())
+
+def tugasMandiri7():
+    a = F(1, 2)
+    b = F(2, 4)
+    c = F(3, 4)
+    a + b
+    print("__add__", a.getNum(), "/", a.getDen())
+    a += b
+    print("__iadd__", a.getNum(), "/", a.getDen())
+    a + c
+    print("__add__", a.getNum(), "/", a.getDen())
+
+def tugasMandiri8():
+    a = F(1, 2)
+    print(a.__repr__())
+
 def tugasMandiri9():
     g = Game("Ganyu", "Shenhe")
     g.play_game()
